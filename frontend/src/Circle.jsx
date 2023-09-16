6f47f46b387967484679231541a1333165e53bd8
mr-fool <mr-fool@users.noreply.github.com>
2023-09-16 16:17:37 -0600

    circle size fixed

--- frontend/src/Circle.jsx
+++ frontend/src/Circle.jsx
@@ -1,33 +1,44 @@
 // src/Circle.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Circle = ({ onClick }) => {
+  const circleSize = Math.random() * 70 + 30; // Random size between 30px and 100px
+
   const [circleStyle, setCircleStyle] = useState({
-    width: `${Math.random() * 100 + 30}px`,
-    height: `${Math.random() * 100 + 30}px`,
+    width: `${circleSize}px`,
+    height: `${circleSize}px`,
     borderRadius: "50%",
-    background: "#00EAD3", // Updated color code
+    background: "#00EAD3",
     position: "absolute",
-    top: `${Math.random() * (window.innerHeight - 100)}px`,
-    left: `${Math.random() * (window.innerWidth - 100)}px`,
     cursor: "pointer",
   });
+
+  const getRandomPosition = () => {
+    const maxX = window.innerWidth - circleSize;
+    const maxY = window.innerHeight - circleSize;
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+    return { left: `${randomX}px`, top: `${randomY}px` };
+  };
 
   const handleClick = () => {
     onClick();
     setCircleStyle((prevStyle) => ({
       ...prevStyle,
-      top: `${Math.random() * (window.innerHeight - 100)}px`,
-      left: `${Math.random() * (window.innerWidth - 100)}px`,
+      ...getRandomPosition(),
     }));
   };
 
+  // Ensure that circles do not overlap initially and after each click.
+  useEffect(() => {
+    setCircleStyle((prevStyle) => ({
+      ...prevStyle,
+      ...getRandomPosition(),
+    }));
+  }, []);
+
   return (
-    <div
-      className="circle"
-      style={circleStyle}
-      onClick={handleClick}
-    ></div>
+    <div className="circle" style={circleStyle} onClick={handleClick}></div>
   );
 };
 
