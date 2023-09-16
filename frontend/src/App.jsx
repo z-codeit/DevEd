13ee62ab1b81f143c996948a42b66a30a7e8ae21
mr-fool <mr-fool@users.noreply.github.com>
2023-09-16 15:03:58 -0600

    added circle changes to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+// src/App.js
+import React, { useState } from "react";
+import Circle from "./Circle";
+import logo from "./logo.png"; // Import the logo image
 
 function App() {
+  const [clicks, setClicks] = useState(0);
+
+  const handleCircleClick = () => {
+    setClicks((prevClicks) => prevClicks + 1);
+  };
+
+  const handleResetClick = () => {
+    setClicks(0);
+  };
 
   return (
-    <>
-    <p>Hello world </p>
-    </>
-  )
+    <div className="App">
+      {/* Display the logo image */}
+      <img src={logo} alt="Logo" />
+      <h1>Aim Trainer</h1>
+      <p>Clicks: {clicks}</p>
+      <div className="circle-container">
+        {[...Array(10)].map((_, index) => (
+          <Circle key={index} onClick={handleCircleClick} spreadArea={400} />
+        ))}
+      </div>
+      <button onClick={handleResetClick}>Reset Score</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
