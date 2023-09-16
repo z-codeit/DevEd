dfa8c003ef117d895e015aa15f9a97da66bdef02
mr-fool <mr-fool@users.noreply.github.com>
2023-09-16 13:38:24 -0600

    circles working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+// src/App.js
+import React, { useState } from "react";
+import Circle from "./Circle";
 
 function App() {
+  const [clicks, setClicks] = useState(0);
+
+  const handleCircleClick = () => {
+    setClicks((prevClicks) => prevClicks + 1);
+  };
 
   return (
-    <>
-    <p>Hello world</p>
-    </>
-  )
+    <div className="App">
+      <h1>Circle Clicker Game</h1>
+      <p>Clicks: {clicks}</p>
+      <div className="circle-container">
+        {[...Array(10)].map((_, index) => (
+          <Circle key={index} onClick={handleCircleClick} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
