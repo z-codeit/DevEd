95b5b164fce753cea7122712dceb4e1197c713f5
Don Vu <33820836+Don-Vu@users.noreply.github.com>
2023-09-16 15:39:07 -0600

    Add configuration for fly.io deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-    <p>Hello world </p>
+    <h1>hello world! Goodbye world!</h1>
     </>
   )
 }
