12f4c82d8138ba647071d717151b766b0a5a3ba3
Dezy Manuel <dezeraymanuel@gmail.com>
2023-09-16 17:11:20 -0600

    added circle code on play click

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.js
 import React, { useState } from "react";
 import Circle from "./Circle";
 import logo from "./logo.png";
@@ -6,8 +5,9 @@ import './App.css';
 
 function App() {
   const [clicks, setClicks] = useState(0);
-  const leaderboard = ["test1", "test2", "test3"];
+  const [playMode, setPlayMode] = useState(false);
 
+  const leaderboard = ["test1", "test2", "test3"];
 
   const handleCircleClick = () => {
     setClicks((prevClicks) => prevClicks + 1);
@@ -17,42 +17,53 @@ function App() {
     setClicks(0);
   };
 
+  const handlePlayClick = () => {
+    setPlayMode(true);
+  };
+
   return (
     <div className="App">
-      <img src={logo} alt="Logo" />
-      <div class='test'>
-      <div class="Title">
-      <h1>Aim Trainer</h1>
-      </div>
-        <div class="LeaderboardTitle">
-        <h3>Leaderboard</h3>
+      {/* Show the landing page only if playMode is false */}
+      {!playMode && (
+        <div className='test'>
+          <div className="Title">
+            <h1>Aim Trainer</h1>
+          </div>
+          <div className="LeaderboardTitle">
+            <h3>Leaderboard</h3>
+          </div>
+          <div className="Leaderboard">
+            <ul>
+              {leaderboard.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="NameField">
+            <form>
+              <input value="Enter name here" onSubmit={() => console.log("hello")}></input>
+            </form>
+          </div>
+          <div className="PlayButton">
+            {/* Show the "Play" button only if playMode is false */}
+            {!playMode && <button type="button" onClick={handlePlayClick}>Play</button>}
+          </div>
         </div>
-        <div class="Leaderboard">
-        <ul>
-          {leaderboard.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-        </div>
-        <div class="NameField">
-        <form>
-          <input value = "Enter name here" onSubmit={() => console.log("hello")}></input>
-        </form>
-        </div>
-        <div class="PlayButton">
-        <button type="button">Play</button>
-        </div>
-      </div>
+      )}
 
-      <p>Clicks: {clicks}</p>
-      <div className="circle-container">
-        {[...Array(10)].map((_, index) => (
-          <Circle key={index} onClick={handleCircleClick} />
-        ))}
-      </div>
-      <button onClick={handleResetClick}>Reset Score</button>
+      {/* Show the circle-related content when playMode is true */}
+      {playMode && (
+        <>
+          <p>Clicks: {clicks}</p>
+          <div className="circle-container">
+            {[...Array(10)].map((_, index) => (
+              <Circle key={index} onClick={handleCircleClick} />
+            ))}
+          </div>
+          <button onClick={handleResetClick}>Reset Score</button>
+        </>
+      )}
     </div>
-    
   );
 }
 
