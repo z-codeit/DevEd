23e722e0fb3ff36ade1d886dd40b8418d3452ace
Divya <d.prasad.8018@gmail.com>
2023-09-16 15:47:38 -0600

    main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const leaderboard = ["test1", "test2", "test3"];
 
   return (
     <>
-    <p>Hello world </p>
+    <div class="Title">
+      <h2> Aim Trainer </h2>
+      </div>
+      <div class="LeaderboardTitle">
+      <h3>Leaderboard</h3>
+      </div>
+      <div class="Leaderboard">
+      <ul>
+        {leaderboard.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      </div>
+      <div class="NameField">
+      <form>
+        <input value = "Enter name here" onSubmit={() => console.log("hello")}></input>
+      </form>
+      </div>
+      <div class="PlayButton">
+      <button type="button">Play</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
