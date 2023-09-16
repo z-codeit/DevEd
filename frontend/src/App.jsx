f1a924f9267e3fd221d3e35a99507b258ef9794e
ARPITA DEB <40507760+ArpitaDeb@users.noreply.github.com>
2023-09-16 17:36:19 -0600

    landing page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,21 @@
 import React, { useState } from "react";
 import Circle from "./Circle";
 import logo from "./logo.png";
-import './App.css';
+import "./App.css";
 
 function App() {
   const [clicks, setClicks] = useState(0);
   const leaderboard = ["test1", "test2", "test3"];
+  const [name, setName] = useState("");
 
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Hello, ${name}!`);
+  };
 
   const handleCircleClick = () => {
     setClicks((prevClicks) => prevClicks + 1);
@@ -20,31 +29,36 @@ function App() {
   return (
     <div className="App">
       <img src={logo} alt="Logo" />
-      <div class='test'>
-      <div class="Title">
-      <h1>Aim Trainer</h1>
+      <div className="test">
+        <div className="Title">
+          <h1>Aim Trainer</h1>
+        </div>
+        <div className="LeaderboardTitle">
+          <h2>Leaderboard</h2>
+        </div>
+        <div className="Leaderboard">
+          <ul>
+            {leaderboard.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="NameField">
+          <form>
+            <input
+              type="text"
+              value={name} // Bind the input value to the name state
+              onChange={handleNameChange} // Add the onChange event handler
+              placeholder="Enter name here"
+              onSubmit={handleSubmit}
+            ></input>
+          </form>
+        </div>
+        <div className="PlayButton">
+          <button type="button">Play</button>
+        </div>
       </div>
-        <div class="LeaderboardTitle">
-        <h3>Leaderboard</h3>
-        </div>
-        <div class="Leaderboard">
-        <ul>
-          {leaderboard.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-        </div>
-        <div class="NameField">
-        <form>
-          <input value = "Enter name here" onSubmit={() => console.log("hello")}></input>
-        </form>
-        </div>
-        <div class="PlayButton">
-        <button type="button">Play</button>
-        </div>
-      </div>
-
-      <p>Clicks: {clicks}</p>
+      <p className="ClickButton">Clicks: {clicks}</p>
       <div className="circle-container">
         {[...Array(10)].map((_, index) => (
           <Circle key={index} onClick={handleCircleClick} />
@@ -52,7 +66,6 @@ function App() {
       </div>
       <button onClick={handleResetClick}>Reset Score</button>
     </div>
-    
   );
 }
 
