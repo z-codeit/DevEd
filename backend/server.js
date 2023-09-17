c334077726de95f9a1461febc7f98bdf84edcd6e
Don Vu <don.p.vu@gmail.com>
2023-09-17 02:33:31 -0600

    Redesign schema

--- backend/server.js
+++ backend/server.js
@@ -7,33 +7,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
 
-const users = {
-  1: {
-    id: 1,
-    username: "user1",
-    score: 10,
-  },
-  2: {
-    id: 2,
-    username: "user2",
-    score: 20,
-  },
-  3: {
-    id: 3,
-    username: "user3",
-    score: 30,
-  },
-  4: {
-    id: 4,
-    username: "user4",
-    score: 40,
-  },
-  5: {
-    id: 5,
-    username: "user5",
-    score: 50,
-  },
-};
+const users = [
+  { name: "John", score: 50 },
+  { name: "Don", score: 34 },
+  { name: "Mike", score: 75 },
+  { name: "Jane", score: 20 },
+  { name: "Sally", score: 60 },
+  { name: "Jill", score: 100 },
+  { name: "Jack", score: 10 },
+  { name: "Jen", score: 30 },
+  { name: "Jenny", score: 80 },
+];
 
 app.get("/api/users", (request, response) => {
   response.json(users);
