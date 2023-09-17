e4a9f510a4b8bd10243ceb6a7e2d85cfe93b6978
Don Vu <33820836+Don-Vu@users.noreply.github.com>
2023-09-16 23:18:30 -0600

    Add summary to end of game

--- frontend/src/pages/play/play.jsx
+++ frontend/src/pages/play/play.jsx
@@ -4,6 +4,9 @@ import "./play.css";
 import Circle from "./Circle";
 
 const Statistics = ({ total, score, time }) => {
+  if (time === 0) {
+    return;
+  }
   let percentage = parseFloat((score / total) * 100).toFixed(1);
   if (percentage === "NaN" || percentage === "Infinity") {
     percentage = 100;
@@ -17,13 +20,16 @@ const Statistics = ({ total, score, time }) => {
   );
 };
 
-const CircleContainer = ({ total, score, setTotal, setScore }) => {
+const CircleContainer = ({ total, score, setTotal, setScore, time }) => {
   const handleCircleClick = () => {
     setScore(score + 1);
     setTotal(total + 1);
     return;
   };
 
+  if (time === 0) {
+    return;
+  }
   return (
     <div>
       {[...Array(Math.floor(Math.random() * (1 - 0 + 1) + 0))].map(
@@ -35,51 +41,80 @@ const CircleContainer = ({ total, score, setTotal, setScore }) => {
   );
 };
 
+const Result = ({ score, total, time }) => {
+  if (time > 0) {
+    return;
+  }
+  return (
+    <div id="result">
+      <h1>Game Over</h1>
+      <h2>Your Score: {score}</h2>
+      <h2>Your Accuracy: {parseFloat((score / total) * 100).toFixed(1)}%</h2>
+    </div>
+  );
+};
+
 function Play() {
   const navigate = useNavigate();
   const [total, setTotal] = useState(0);
   const [score, setScore] = useState(0);
-  const [time, setTime] = useState(60);
+  const [time, setTime] = useState(10);
 
   useEffect(() => {
+    if (time === 0) {
+      return;
+    }
     setTimeout(() => {
       setTime(time - 1);
     }, 1100);
   }, [time]);
 
+  const handleMissClick = (event) => {
+    if (time === 0) {
+      return;
+    }
+    setTotal(total + 1);
+  };
+
   return (
-    <div className="container2" onClick={() => setTotal(total + 1)}>
-      <CircleContainer
-        total={total}
-        score={score}
-        setTotal={setTotal}
-        setScore={setScore}
-      />
-      <CircleContainer
-        total={total}
-        score={score}
-        setTotal={setTotal}
-        setScore={setScore}
-      />
-      <CircleContainer
-        total={total}
-        score={score}
-        setTotal={setTotal}
-        setScore={setScore}
-      />
-      <CircleContainer
-        total={total}
-        score={score}
-        setTotal={setTotal}
-        setScore={setScore}
-      />
-      <CircleContainer
-        total={total}
-        score={score}
-        setTotal={setTotal}
-        setScore={setScore}
-      />
+    <div className="container2" onClick={handleMissClick}>
       <Statistics total={total} score={score} time={time} />
+      <Result score={score} total={total} time={time} />
+      <CircleContainer
+        total={total}
+        score={score}
+        setTotal={setTotal}
+        setScore={setScore}
+        time={time}
+      />
+      <CircleContainer
+        total={total}
+        score={score}
+        setTotal={setTotal}
+        setScore={setScore}
+        time={time}
+      />
+      <CircleContainer
+        total={total}
+        score={score}
+        setTotal={setTotal}
+        setScore={setScore}
+        time={time}
+      />
+      <CircleContainer
+        total={total}
+        score={score}
+        setTotal={setTotal}
+        setScore={setScore}
+        time={time}
+      />
+      <CircleContainer
+        total={total}
+        score={score}
+        setTotal={setTotal}
+        setScore={setScore}
+        time={time}
+      />
     </div>
   );
 }
