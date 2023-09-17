19a8f1d3eeeb2994b5f48a788b1a2a1067b1c886
Don Vu <don.p.vu@gmail.com>
2023-09-17 03:21:01 -0600

    Handle POST to api/users

--- backend/server.js
+++ backend/server.js
@@ -7,25 +7,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
 
-const users = [
-  { name: "John", score: 50 },
-  { name: "Don", score: 34 },
-  { name: "Mike", score: 75 },
-  { name: "Jane", score: 20 },
-  { name: "Sally", score: 60 },
-  { name: "Jill", score: 100 },
-  { name: "Jack", score: 10 },
-  { name: "Jen", score: 30 },
-  { name: "Jenny", score: 80 },
-];
+const users = [];
 
 app.post("/api/users", (request, response) => {
   const body = request.body;
-  // if (!body.name || !body.score) {
-  //   return response.status(400).json({
-  //     error: "content missing",
-  //   });
-  // }
   let user = {
     name: body.name,
     score: body.score,
